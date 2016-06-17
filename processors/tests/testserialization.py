509e34231a18d187b1cbb146ea08ed0b9c433c0b
Gus Hahn-Powell <gushahnpowell@gmail.com>
2016-06-17 00:10:25 -0700

    Tests for new serialization format

--- processors/tests/testserialization.py
+++ processors/tests/testserialization.py
@@ -9,7 +9,7 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 
 class DocumentSerializationTests(unittest.TestCase):
 
-    def test_load_from_json(self):
+    def test_doc_load_from_json(self):
         "Document.load_from_JSON should produce a Document from serialized_doc.json"
         json_file = os.path.join(__location__, "serialized_doc.json")
         print(json_file)
@@ -17,5 +17,13 @@ class DocumentSerializationTests(unittest.TestCase):
             doc = Document.load_from_JSON(json.load(jf))
         self.assertTrue(isinstance(doc, Document), "Document.load_from_JSON did not produce a Document from {}".format(json_file))
 
+    def test_sentence_load_from_json(self):
+        "Sentence.load_from_JSON should produce a Sentence from serialized_sentence.json"
+        json_file = os.path.join(__location__, "serialized_sentence.json")
+        print(json_file)
+        with open(json_file, "r") as jf:
+            s = Sentence.load_from_JSON(json.load(jf))
+        self.assertTrue(isinstance(s, Sentence), "Sentence.load_from_JSON did not produce a Sentence from {}".format(json_file))
+        
 if __name__ == "__main__":
     unittest.main()
