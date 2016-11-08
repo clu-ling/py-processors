a9f020587f71455a36c10f89d3811b124d48f18a
Gus Hahn-Powell <gushahnpowell@gmail.com>
2016-11-07 22:51:16 -0700

    Version 3.0

--- processors/__init__.py
+++ processors/__init__.py
@@ -11,6 +11,6 @@ import json
 
 __title__ = 'py-processors'
 __version__ = '2.9.7'
-__ps_rec__ = '2.9' # known compatible version of server
+__ps_rec__ = '3.0' # known compatible version of server
 __author__ = 'Gus Hahn-Powell'
 __copyright__ = 'Copyright 2016 Gus Hahn-Powell'
